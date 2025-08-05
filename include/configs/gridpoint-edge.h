508793d7a0fbccd98b4c4a3ec3ad3ea9dc996e6d
abelino <abelino.romo@gmail.com>
2025-08-05 14:50:52 -0700

    Add edge board support

---
+++ include/configs/gridpoint-edge.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration file for the SAMA5D27 SOM1 EK Board.
+ *
+ * Copyright (C) 2017 Microchip Corporation
+ *		      Wenyou Yang <wenyou.yang@microchip.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "at91-sama5_common.h"
+
+#undef CONFIG_SYS_AT91_MAIN_CLOCK
+#define CONFIG_SYS_AT91_MAIN_CLOCK      24000000 /* from 24 MHz crystal */
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SYS_INIT_SP_ADDR     0x218000
+#elif CONFIG_CLK_SCMI
+#define CONFIG_SYS_INIT_SP_ADDR \
+    (0x22000000 + 16 * 1024 + CONFIG_SYS_MALLOC_F_LEN - \
+     GENERATED_GBL_DATA_SIZE)
+#else
+#define CONFIG_SYS_INIT_SP_ADDR \
+    (0x20000000 + 16 * 1024 + CONFIG_SYS_MALLOC_F_LEN - \
+     GENERATED_GBL_DATA_SIZE)
+#endif
+
+/* Ethernet Hardware */
+#define CONFIG_RMII
+#define CONFIG_NET_RETRY_COUNT		20
+#define CONFIG_MACB_SEARCH_PHY
+
+/* SDRAM */
+#define CFG_SYS_SDRAM_BASE		0x20000000
+#define CFG_SYS_SDRAM_SIZE		0x10000000
+
+/* SPL */
+#define CONFIG_SYS_SPL_MALLOC_START	0x20080000
+#define CONFIG_SYS_SPL_MALLOC_SIZE	0x80000
+
+#endif
